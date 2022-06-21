0dc5bfb6696ad75d78d4ac556ac119ed02d2194b
Aman Kaushik <amankaushik481@gmail.com>
2022-06-21 18:17:58 +0530

    desktop complete

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import "./hero.css";
+import Globe from "../../assets/globe.svg";
+import Blob from "../../assets/Vector.svg";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="hero">
+      <div className="text__container">
+        <img className="blob" src={Blob} />
+        <h1 className="heading">
+          We give more than <span className="span">140%</span> returns on your
+          fortune
+        </h1>
+        <a className="know__link" href="/whitepaper">
+          <button className="know">Know How</button>
+        </a>
+        <button className="invest">Invest Now</button>
+      </div>
+      <div className="globe__container">
+        <img className="globe" src={Globe} />
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import "./navbar.css";
+import Logo from "../../assets/logo.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="logo__container">
+        <img className="logo" src={Logo} />
+      </div>
+      <div className="navlist__container">
+        <ul>
+          <li>
+            <a href="/about">About Us</a>
+          </li>
+          <li>
+            <a href="/vision">Vision</a>
+          </li>
+          <li>
+            <a href="/roadmap">The Roadmap</a>
+          </li>
+          <li>
+            <a href="/whitepaper">Whitepaper</a>
+          </li>
+          <li>
+            <button>Get in touch</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
