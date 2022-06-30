ff42f2a62b38d176725a3efebcc6134a8e365139
Aman Kaushik <amankaushik481@gmail.com>
2022-06-30 18:31:10 +0530

    new ui revamp

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,27 +8,19 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="text__container">
-        <h1>About</h1>
-        <p>
-          Dimension is a proof-of-stake masternode mining protocol which enables
-          staking mechanism that is a one click investment platform, where users
-          will be able to stake their tokens to get a high yield interest of
-          around 80% to 100% per annum.
-          <br />
-          <br />
-          Dimension Protocol help other proof-of-stake cryptocurrencys by
-          increasing their efficiency in validation process which help them
-          increase their transaction speed and limits.
-          <br />
-          <br />
-          Dimension Protocol is a complex masternode mining program which is a
-          blockchain based economy. This protocol generates revenue by operating
-          several masternodes, price gains and services provided leveraging each
-          cryptocurrency's blockchain.
-        </p>
+        <h1>
+          We give more than{" "}
+          <span className="span" s>
+            140%
+          </span>{" "}
+          returns on your fortune
+        </h1>
+        <div className="button__container">
+          <button className="know">Know How</button>
+          <button className="invest">Invest Now</button>
+        </div>
       </div>
       <div className="globe__container">
-        {/* <img className="globe" src={Logo} /> */}
         <img className="globe" src={Globe} />
       </div>
     </div>
